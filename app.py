6bd4bec43eaf3bbc03b3170293df2caa48255ff6
Amogh Dambal <amdam2013@gmail.com>
2020-01-13 20:24:35 -0600

    added tweet embedding functionality

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 
 from filter import authenticate
 from tweet import Tweet
+from oembedder import get_oembed
 
 app = Flask(__name__)
 
@@ -20,15 +21,18 @@ def parse():
 
 	if api is not None:
 		fetched_tweets = [Tweet(t) for t in api.user_timeline(id=user, count=count)]
+		for ft in fetched_tweets:
+			print(ft)
 		fetched_tweets.sort(key=lambda t: t.likes, reverse=True)
 		fetched_tweets = [tweet for tweet in fetched_tweets if not tweet.is_retweet]
+		embeddable_tweets = [get_oembed(tweet) for tweet in fetched_tweets]
 	else:
 		fetched_tweets = None
 
 	if fetched_tweets is None:
 		return render_template('error.html')
 	else:
-		return render_template('tweets.html', tweets=fetched_tweets)
+		return render_template('tweets.html', emb=embeddable_tweets)
 
 
 if __name__ == '__main__':
---
+++ oembedder.py
@@ -0,0 +1,32 @@
+# python 3.6
+# tool to handle making GET requests to Twitter API
+# for oembed compatible data
+import requests
+import pprint
+
+
+def get_oembed(tweet):
+	"""
+	convert a tweepy Status object into a oembeddable tweet
+	using Twitter API
+	:param tweet: a Tweet object
+	:return: json data for that representation of the tweet
+	"""
+
+	# api access point
+	endpoint = "https://publish.twitter.com/oembed"
+
+	# screen name
+	screen_name = tweet.json['user']['screen_name']
+	# id_str
+	id_str = tweet.json['id_str']
+	# build url of tweet
+	url = f"http://twitter.com/{screen_name}/status/{id_str}"
+
+	params = {
+		'url': url,
+		'omit_script': True
+	}
+
+	r = requests.get(url=endpoint, params=params)
+	return r.json()
